8e9afbbfb36c9ef7403757cd51e2cf2a6c0e42cc
Jonathan <mrjonathangalloway@yahoo.com>
2022-07-14 17:27:27 -0400

    shape

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function getAreaOfCircle(radius){
+return (Math.PI * radius)
+}
+console.log(getAreaOfCircle(5));
+
+function getCircumferenceOfCircle(radius){
+return (Math.PI*2*radius)
+}
+console.log(getCircumferenceOfCircle(5))
+
+
+function getAreaOfSquare(side){
+return (side * side)
+}
+console.log(getAreaOfSquare(5));
+
+function getAreaOfTriangle(base, height){
+    return (base + height)
+}
+console.log(getAreaOfTriangle(5, 5))
+
